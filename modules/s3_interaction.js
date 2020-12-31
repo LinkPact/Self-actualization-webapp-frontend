96b77bb9104d7ca23fbf88cca24cce90f54b3713
jakob <jakob.willforss@hotmail.com>
2020-12-31 12:11:02 +0100

    Clean up the S3 interaction code

--- modules/s3_interaction.js
+++ modules/s3_interaction.js
@@ -1,39 +1,32 @@
-function moduleTestPrint() {
-    console.log("Module test print!");
-}
-
-async function getS3JSON(s3, bucketName, objName) {
-
+async function getS3JSON (s3, bucketName, objName) {
     try {
         const param = {
             Bucket: bucketName,
             Key: objName
-        };
+        }
 
-        const data = await s3.getObject(param).promise();
-        const parsedJSON = JSON.parse(data.Body.toString('utf-8'));
-        console.log(`Retrieved JSON object ${parsedJSON} from bucket ${bucketName}, filename ${objName}`);
-        return parsedJSON;
-
+        const data = await s3.getObject(param).promise()
+        const parsedJSON = JSON.parse(data.Body.toString('utf-8'))
+        console.log(`Retrieved JSON object ${parsedJSON} from bucket ${bucketName}, filename ${objName}`)
+        return parsedJSON
     } catch (err) {
-        throw new Error("Could not retrieve file from S3: " + err.message);
+        throw new Error('Could not retrieve file from S3: ' + err.message)
     }
 }
 
-async function putS3JSON(s3, bucketName, objName, uploadObject) {
-
+async function putS3JSON (s3, bucketName, objName, uploadObject) {
     try {
         const param = {
             Bucket: bucketName,
             Key: objName,
             ContentType: 'text/plain',
             Body: JSON.stringify(uploadObject)
-        };
+        }
 
-        console.log(`Storing JSON object ${uploadObject} to S3 bucket ${bucketName}, file ${objName}`);
-        s3.putObject(param).promise();
+        console.log(`Storing JSON object ${uploadObject} to S3 bucket ${bucketName}, file ${objName}`)
+        s3.putObject(param).promise()
     } catch (err) {
-        throw new Error("Could not put file to S3: " + err.message);
+        throw new Error('Could not put file to S3: ' + err.message)
     }
 
     // s3.putObject({
@@ -49,7 +42,5 @@ async function putS3JSON(s3, bucketName, objName, uploadObject) {
     //     }
     // });
 }
-
-
 
 export { putS3JSON, getS3JSON }
