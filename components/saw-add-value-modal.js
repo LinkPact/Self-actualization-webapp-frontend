ddb216d253f90bacb33f847bb784e7b00ec87a54
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-01 10:15:46 +0100

    Modal: Recreates previous modal html structure
    Added manually with createElement and appendChild for now. Will
    look into how to use a template later.

--- components/saw-add-value-modal.js
+++ components/saw-add-value-modal.js
@@ -9,7 +9,38 @@ class SawAddValueModal extends HTMLElement {
 
     _createModal () {
         const dialog = document.createElement('paper-dialog')
-        dialog.appendChild(document.createTextNode('hello world'))
+        const wrapper = document.createElement('div')
+        const title = document.createElement('h3')
+        const nameLabel = document.createElement('label')
+        const nameInput = document.createElement('input')
+        const descLabel = document.createElement('label')
+        const descInput = document.createElement('textarea')
+        const addButton = document.createElement('button')
+
+        title.appendChild(document.createTextNode('Add Value'))
+        nameLabel.setAttribute('for', 'value_name_input')
+        nameLabel.appendChild(document.createTextNode('Name: '))
+        nameLabel.setAttribute('id', 'value_name_input')
+        nameInput.setAttribute('type', 'text')
+        nameInput.setAttribute('name', 'Input value name')
+        descLabel.setAttribute('for', 'value_description_input')
+        descLabel.appendChild(document.createTextNode('Description: '))
+        descInput.setAttribute('id', 'value_description_input')
+        addButton.appendChild(document.createTextNode('Add value'))
+
+        // TODO: Pass callback with attribute
+        addButton.addEventListener('click', () => {
+            console.log('clicked')
+        })
+
+        nameLabel.appendChild(nameInput)
+        descLabel.appendChild(descInput)
+        wrapper.appendChild(title)
+        wrapper.appendChild(nameLabel)
+        wrapper.appendChild(descLabel)
+        wrapper.appendChild(addButton)
+        dialog.appendChild(wrapper)
+
         return dialog
     }
 }
