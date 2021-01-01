79e8f102997c1dd18adc8516fa174d8e080ca4f5
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-01 20:06:14 +0100

    Modal: Adds input field prefill attributes
    Adds two attributes 'prefill-name' and 'prefill-description' that
    allows the caller to prefill the modal's name and description
    input fields.
    
    The reason is to make it possible for the same modal to be reused
    for both adding and editing a value.

--- experiments/6_web_component/main.js
+++ experiments/6_web_component/main.js
@@ -1,10 +1,21 @@
 import '../../components/upsert-value-modal.js'
 
-document.getElementById('open-upsert-value-modal-button').addEventListener('click', () => {
+document.getElementById('open-add-value-modal-button').addEventListener('click', () => {
     const modal = document.createElement('saw-upsert-value-modal')
     modal.addEventListener('saw.modal-submit', (e) => {
         console.log(e.detail.input)
     })
     modal.addEventListener('saw.modal-close', (e) => document.body.removeChild(modal))
+    document.body.appendChild(modal)
+})
+
+document.getElementById('open-edit-value-modal-button').addEventListener('click', () => {
+    const modal = document.createElement('saw-upsert-value-modal')
+    modal.addEventListener('saw.modal-submit', (e) => {
+        console.log(e.detail.input)
+    })
+    modal.addEventListener('saw.modal-close', (e) => document.body.removeChild(modal))
+    modal.setAttribute('prefill-name', 'TestPrefillName')
+    modal.setAttribute('prefill-description', 'TestPrefillDescription')
     document.body.appendChild(modal)
 })
