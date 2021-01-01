8fb6eecf5a4cfe6c60c7aeabe4fd2f78c1dc82bc
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-01 15:51:29 +0100

    Modal: Use template to define HTML-tags and style

--- components/saw-add-value-modal.js
+++ components/saw-add-value-modal.js
@@ -10,62 +10,56 @@ class SawAddValueModal extends HTMLElement {
     constructor () {
         super()
         this.attachShadow({ mode: 'open' })
+        this.shadowRoot.innerHTML = `
+            <style>
+                label {
+                    display: block;
+                }
+
+                textarea {
+                    display: block;
+                }
+            </style>
+
+            <paper-dialog id="dialog">
+                <div>
+                    <h3>Add Value</h3>
+
+                    <label for="name-input">
+                        Name: <input type="text" id="name-input" />
+                    </label>
+
+                    <label for="description-input">
+                        Description:
+                        <textarea id="description-input"></textarea>
+                    </label>
+
+                    <button id="submit-button">Add value</button>
+                </div>
+            </paper-dialog>
+        `
     }
 
     connectedCallback () {
-        const dialog = this._createModal()
-        this.shadowRoot.appendChild(dialog)
-        dialog.open()
-    }
+        const dialog = this.shadowRoot.getElementById('dialog')
 
-    _createModal () {
-        const dialog = document.createElement('paper-dialog')
-        const wrapper = document.createElement('div')
-        const title = document.createElement('h3')
-        const nameLabel = document.createElement('label')
-        const nameInput = document.createElement('input')
-        const descLabel = document.createElement('label')
-        const descInput = document.createElement('textarea')
-        const addButton = document.createElement('button')
-
-        title.appendChild(document.createTextNode('Add Value'))
-        nameLabel.setAttribute('for', 'value_name_input')
-        nameLabel.appendChild(document.createTextNode('Name: '))
-        nameLabel.setAttribute('id', 'value_name_input')
-        nameInput.setAttribute('type', 'text')
-        nameInput.setAttribute('name', 'Input value name')
-        descLabel.setAttribute('for', 'value_description_input')
-        descLabel.appendChild(document.createTextNode('Description: '))
-        descInput.setAttribute('id', 'value_description_input')
-        addButton.appendChild(document.createTextNode('Add value'))
-
-        nameLabel.style.display = 'block'
-        descInput.style.display = 'block'
-
-        addButton.addEventListener('click', () => {
+        this.shadowRoot.getElementById('submit-button').addEventListener('click', () => {
             this.dispatchEvent(new CustomEvent('on-submit', {
                 detail: {
                     input: {
-                        name: nameInput.value,
-                        description: descInput.value
+                        name: this.shadowRoot.getElementById('name-input').value,
+                        description: this.shadowRoot.getElementById('description-input').value
                     }
                 }
             }))
+
             dialog.close()
         })
 
         dialog.addEventListener('iron-overlay-closed', () =>
             this.dispatchEvent(new CustomEvent('on-close')))
 
-        nameLabel.appendChild(nameInput)
-        descLabel.appendChild(descInput)
-        wrapper.appendChild(title)
-        wrapper.appendChild(nameLabel)
-        wrapper.appendChild(descLabel)
-        wrapper.appendChild(addButton)
-        dialog.appendChild(wrapper)
-
-        return dialog
+        dialog.open()
     }
 }
 
