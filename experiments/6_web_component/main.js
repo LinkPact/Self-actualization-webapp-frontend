baab4c143074565473d4b55f74e491ebecb1e460
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-02 07:07:58 +0100

    Adds upsert habit modal
    Works the same as the value equivalent for now, except that it
    takes 'prefill-values', a comma-separated list of values to
    prefill the 'Values: ' field with, instead of 'prefill-description'

--- experiments/6_web_component/main.js
+++ experiments/6_web_component/main.js
@@ -1,4 +1,5 @@
 import '../../components/upsert-value-modal.js'
+import '../../components/upsert-habit-modal.js'
 
 document.getElementById('open-add-value-modal-button').addEventListener('click', () => {
     const modal = document.createElement('saw-upsert-value-modal')
@@ -17,5 +18,25 @@ document.getElementById('open-edit-value-modal-button').addEventListener('click'
     modal.addEventListener('saw.modal-close', (e) => document.body.removeChild(modal))
     modal.setAttribute('prefill-name', 'TestPrefillName')
     modal.setAttribute('prefill-description', 'TestPrefillDescription')
+    document.body.appendChild(modal)
+})
+
+document.getElementById('open-add-habit-modal-button').addEventListener('click', () => {
+    const modal = document.createElement('saw-upsert-habit-modal')
+    modal.addEventListener('saw.modal-submit', (e) => {
+        console.log(e.detail.input)
+    })
+    modal.addEventListener('saw.modal-close', (e) => document.body.removeChild(modal))
+    document.body.appendChild(modal)
+})
+
+document.getElementById('open-edit-habit-modal-button').addEventListener('click', () => {
+    const modal = document.createElement('saw-upsert-habit-modal')
+    modal.addEventListener('saw.modal-submit', (e) => {
+        console.log(e.detail.input)
+    })
+    modal.addEventListener('saw.modal-close', (e) => document.body.removeChild(modal))
+    modal.setAttribute('prefill-name', 'TestPrefillName')
+    modal.setAttribute('prefill-values', 'TestValue1, TestValue2, TestValue3')
     document.body.appendChild(modal)
 })
