ff4c1ffbcd6562429e2b476b7fa976c2c5c44401
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-21 17:19:20 +0100

    Adds collapse for notes inside value-info-modal

--- components/value-info-modal.js
+++ components/value-info-modal.js
@@ -1,6 +1,11 @@
 import 'https://unpkg.com/@polymer/paper-dialog/paper-dialog.js?module'
 
 /*
+ * TODO: Need to redesign this so it is automatically updated when the 'opened'
+ * attribute on the iron-collapse element is toggled.
+ */
+
+/*
  * Events:
  * - saw.modal-close                    dispatched when the modal is closed
  * - saw.valueinfomodal-delete-click    dispatched when the user clicks on the delete value
@@ -21,8 +26,12 @@ class ValueInfoModal extends HTMLElement {
             <paper-dialog id="dialog">
                 <h3 id="title"></h3>
                 <p id="description"></p>
-                <p id="note"></p>
                 <ul id="habits"></ul>
+
+                <button id="notes-toggle">Display Notes</h4>
+                <iron-collapse id="collapse-area">
+                    <ul id="notes"></ul>
+                </iron-collapse>
 
                 <button id="delete_button">delete</button>
                 <button id="edit_button">edit</button>
@@ -54,22 +63,39 @@ class ValueInfoModal extends HTMLElement {
     }
 
     connectedCallback () {
-        const deleteButton = this.shadowRoot.querySelector('#delete_button')
-        const editButton = this.shadowRoot.querySelector('#edit_button')
-
         this.shadowRoot.querySelector('#title').innerHTML = this._value.name
 
         if (this._value.description) {
             this.shadowRoot.querySelector('#description').innerHTML = this._value.description
         }
 
-        if (this._value.note) {
-            this.shadowRoot.querySelector('#note').innerHTML = this._value.note
-        }
+        this._value.notes?.forEach(note => (
+            this.shadowRoot.querySelector('#notes').appendChild(this._createNoteListItem(note))
+        ))
 
         this._habits.forEach(habit =>
             this.shadowRoot.querySelector('#habits').appendChild(this._createHabitListItem(habit))
         )
+
+        this._dialog.open()
+    }
+
+    _createHabitListItem (habit) {
+        const li = document.createElement('li')
+        li.innerHTML = habit.name
+        return li
+    }
+
+    _createNoteListItem (note) {
+        const li = document.createElement('li')
+        li.innerHTML = note
+        return li
+    }
+
+    _setupEventListeners () {
+        const deleteButton = this.shadowRoot.querySelector('#delete_button')
+        const editButton = this.shadowRoot.querySelector('#edit_button')
+        const expandNotesButton = this.shadowRoot.querySelector('#')
 
         deleteButton.addEventListener('click', e => {
             this.dispatchEvent(new CustomEvent('saw.valueinfomodal-delete-click', {
@@ -88,16 +114,12 @@ class ValueInfoModal extends HTMLElement {
             }))
         })
 
+        expandNotesButton.addEventListener('click', e => (
+            this.querySelector('#collapse-area').toggleAttribute('opened')
+        ))
+
         this._dialog.addEventListener('iron-overlay-closed', () =>
             this.dispatchEvent(new CustomEvent('saw.modal-close')))
-
-        this._dialog.open()
-    }
-
-    _createHabitListItem (habit) {
-        const li = document.createElement('li')
-        li.innerHTML = habit.name
-        return li
     }
 }
 
